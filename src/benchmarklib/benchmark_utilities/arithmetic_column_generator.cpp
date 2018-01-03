35fa6d725f2fa313fea790eb78ed44e4eb13000b
Max Jendruk <code@jendruk.com>
2018-01-03 18:30:05 +0100

    Add arithmetic column generator

---
+++ src/benchmarklib/benchmark_utilities/arithmetic_column_generator.cpp
@@ -0,0 +1,90 @@
+#include "arithmetic_column_generator.hpp"
+
+#include <random>
+#include <memory>
+
+#include "resolve_type.hpp"
+#include "storage/value_column.hpp"
+
+
+namespace benchmark_utilities {
+
+namespace {
+
+template <typename T, std::enable_if_t<std::is_integral_v<T>>* = nullptr>
+auto get_uniform_dist(const T min, const T max) {
+  return std::uniform_int_distribution{min, max};
+}
+
+template <typename T, std::enable_if_t<std::is_floating_point_v<T>>* = nullptr>
+auto get_uniform_dist(const T min, const T max) {
+  return std::uniform_real_distribution{min, max};
+}
+
+}  // namespace
+
+using namespace opossum;
+
+template <typename T>
+ArithmeticColumnGenerator<T>::ArithmeticColumnGenerator(PolymorphicAllocator<size_t> alloc)
+    : _data_type{data_type_from_type<T>()},
+      _alloc{alloc},
+      _row_count{4'000'000} {}
+
+template <typename T>
+void ArithmeticColumnGenerator<T>::set_row_count(const uint32_t row_count) {
+  _row_count = row_count;
+}
+
+template <typename T>
+std::shared_ptr<ValueColumn<T>> ArithmeticColumnGenerator<T>::uniformly_distributed_column(const T min, const T max) {
+  auto values = pmr_concurrent_vector<T>(_row_count, _alloc);
+
+  std::mt19937 gen{};
+  auto dist = get_uniform_dist(min, max);
+
+  for (auto i = 0u; i < _row_count; ++i) {
+    values[i] = dist(gen);
+  }
+
+  return column_from_values(std::move(values));
+}
+
+template <typename T>
+std::shared_ptr<ValueColumn<T>> ArithmeticColumnGenerator<T>::normally_distributed_column(const T mean, const T std_dev, std::optional<OutlierParams> outlier_params) {
+  auto values = pmr_concurrent_vector<T>(_row_count, _alloc);
+
+  std::mt19937 gen{};
+  auto dist = std::normal_distribution<double>(mean, std_dev);
+
+  if (!outlier_params) {
+    for (auto i = 0u; i < _row_count; ++i) {
+      values[i] = dist(gen);
+    }
+  } else {
+    auto is_outlier_dist = std::bernoulli_distribution(outlier_params->fraction);
+    auto outlier_dist = std::normal_distribution<double>(outlier_params->mean, outlier_params->std_dev);
+
+    for (auto i = 0u; i < _row_count; ++i) {
+      if (is_outlier_dist(gen)) {
+        values [i] = outlier_dist(gen);
+      } else {
+        values[i] = dist(gen);
+      }
+    }
+  }
+
+  return column_from_values(std::move(values));
+}
+
+template <typename T>
+std::shared_ptr<ValueColumn<T>> ArithmeticColumnGenerator<T>::column_from_values(pmr_concurrent_vector<T> values) {
+  return std::allocate_shared<ValueColumn<T>>(_alloc, std::move(values));
+}
+
+template class ArithmeticColumnGenerator<int32_t>;
+template class ArithmeticColumnGenerator<int64_t>;
+template class ArithmeticColumnGenerator<float>;
+template class ArithmeticColumnGenerator<double>;
+
+}  // namespace benchmark_utilities
---
+++ src/benchmarklib/benchmark_utilities/arithmetic_column_generator.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <cstdint>
+#include <memory>
+#include <optional>
+#include <type_traits>
+
+#include "all_type_variant.hpp"
+#include "types.hpp"
+
+namespace opossum {
+
+template <typename T>
+class ValueColumn;
+
+}  // namespace opossum
+
+namespace benchmark_utilities {
+
+template <typename T>
+class ArithmeticColumnGenerator {
+  static_assert(std::is_arithmetic_v<T>);
+
+ public:
+  ArithmeticColumnGenerator(opossum::PolymorphicAllocator<size_t> alloc);
+
+  void set_row_count(const uint32_t row_count);
+
+  std::shared_ptr<opossum::ValueColumn<T>> uniformly_distributed_column(const T min, const T max);
+
+  struct OutlierParams {
+    const double fraction;
+    const T mean;
+    const T std_dev;
+  };
+
+  std::shared_ptr<opossum::ValueColumn<T>> normally_distributed_column(
+      const T mean, const T std_dev, std::optional<OutlierParams> outlier_params);
+
+ private:
+  std::shared_ptr<opossum::ValueColumn<T>> column_from_values(opossum::pmr_concurrent_vector<T> values);
+
+ private:
+  const opossum::DataType _data_type;
+  const opossum::PolymorphicAllocator<size_t> _alloc;
+  uint32_t _row_count;
+};
+
+}  // benchmark_utilities
